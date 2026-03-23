13d841648786d4674530817fd6c3fd07ab5ef4de
Tontonchikc <prangantonn@gmail.com>
2026-03-23 07:05:58 +0200

    hw9

--- js/hw9.js
+++ js/hw9.js
@@ -6,11 +6,6 @@ function logItems(array) {
 
 logItems(["Mango", "Kivi", "Ball"]);
 
-
-
-
-
-
 let sum = 0;
 
 function calculateEngravingPrice(message, pricePerLetter) {
@@ -25,41 +20,84 @@ function calculateEngravingPrice(message, pricePerLetter) {
 
 console.log(calculateEngravingPrice("Anton Sasha Misha", 5));
 
-
-
-
-let longestWord;
-
 function findLongestWord(words) {
-   let wordsArray = words.split
-  for (let i = 0; i < wordsArray.lentgh; i++) {
-    if(wordsArray[i].length > longestWord.length){
-        longestWord = wordsArray[i]
+  const wordsArray = words.split(" ");
+  let longestWord = "";
+
+  for (let i = 0; i < wordsArray.length; i++) {
+    if (wordsArray[i].length > longestWord.length) {
+      longestWord = wordsArray[i];
     }
   }
-  return longestWord
+
+  return longestWord;
 }
 
 console.log(findLongestWord("String Number Boolean"));
 
-
-
-
-
-
-
 function formatString(string) {
-    if (string.length < 40) {
-        console.log(string)
-    }
-    else { console.log(string.slice(39, 99999999999, "...")) }
-
+  if (string.length <= 40) {
+    return string;
+  } else {
+    return string.slice(0, 40) + "...";
+  }
 }
 
-formatString("Hello my names Anton,I am from Kiyv bla bla bla");
+console.log(formatString("Hello my names Anton,I am from Kiyv bla bla bla"));
 
 
 
 
 
 
+function checkForSpam(message) {
+
+  const normalizedMessage = message.toLowerCase();
+
+  if (normalizedMessage.includes("spam") || normalizedMessage.includes("sale")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+console.log(checkForSpam("this is SPAM message,i want SaLe")); 
+
+
+
+
+
+const numbers = [];
+let input;
+
+while (true) {
+  input = prompt("Введіть число:");
+
+  if (input === null) {
+
+    break;
+  }
+
+  const number = Number(input);
+
+  if (isNaN(number)) {
+    alert("Було введено не число, попробуйте ще раз");
+    continue; 
+  }
+
+  numbers.push(number); 
+}
+
+
+if (numbers.length > 0) {
+  let total = 0;
+
+  for (const num of numbers) {
+    total += num;
+  }
+
+  console.log(`Загальна сума чисел дорівнює ${total}`);
+} else {
+  console.log("Масив чисел порожній");
+}
