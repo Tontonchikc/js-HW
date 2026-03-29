33022fc2ab87375852151b45e1490f20f7da4449
Tontonchikc <prangantonn@gmail.com>
2026-03-29 21:57:52 +0300

    blabla

---
+++ js/hw11.js
@@ -0,0 +1,12 @@
+const bankAccount = {
+    ownerName: "Sam",
+    accountNumber: 467,
+    balance: 350,
+
+
+    
+deposit() {
+     const addMoney = prompt("Введіть суму яку ви хочете додати до рахунку")
+    addMoney
+    }
+}
