cd421e9e98f6eb996fc5959f3463fdda9f3e0fd0
Tontonchikc <prangantonn@gmail.com>
2026-04-03 16:34:54 +0300

    hw11

--- js/hw11.js
+++ js/hw11.js
@@ -72,3 +72,67 @@ const weather = {
 const result = weather.weatherTemperature();
 
 console.log("Результат:", result);
+
+
+
+
+
+
+
+
+const user = {
+    name: prompt("Введіть ваше ім'я"),
+    email: prompt("Створіть email"),
+    password: prompt("Створіть пароль"),
+
+    login() {
+        const inputEmail = prompt("Введіть email для входу");
+        const inputPassword = prompt("Введіть пароль");
+
+        if (!inputEmail.includes("@")) {
+            alert("Email повинен містити '@'");
+            return;
+        }
+
+        if (inputEmail === this.email && inputPassword === this.password) {
+            alert("Вхід успішний");
+        } else {
+            alert("Невірний email або пароль");
+        }
+    }
+};
+
+user.login();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const movie = {
+    title: "Inception",
+    director: "Christopher Nolan",
+    year: 2010,
+    rating: 8.8,
+
+    isHighRated() {
+        return this.rating > 8;
+    }
+};
+
+
+console.log("Назва:", movie.title);
+console.log("Режисер:", movie.director);
+console.log("Рік:", movie.year);
+console.log("Рейтинг:", movie.rating);
+
+
+console.log("Рейтинг вище 8?", movie.isHighRated());
